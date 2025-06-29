34b152e13f7b2f637c370dd1d85f9fc61d2b28a1
Kadi <127500460+adilkadivala@users.noreply.github.com>
2025-06-29 10:40:25 +0530

    Updated layout.tsx

--- client/app/dashboard/layout.tsx
+++ client/app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       >
         {children}
       </body>
